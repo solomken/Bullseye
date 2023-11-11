08c03dc33d8248ce2a9ee74387d7e163e516ddd0
Anastasiia Solomka <solomken@gmail.com>
2023-11-11 14:11:45 +0100

    extract views in ContentView in separate Views

---
+++ Bullseye/views/TextViews.swift
@@ -0,0 +1,18 @@
+//
+//  TextViews.swift
+//  Bullseye
+//
+//  Created by Anastasiia Solomka on 11.11.2023.
+//
+
+import SwiftUI
+
+struct TextViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextViews()
+}
