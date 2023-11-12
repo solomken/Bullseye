18eea15bd6cf8b728c4de0a4b77a3de7515f1560
Anastasiia Solomka <solomken@gmail.com>
2023-11-12 09:44:22 +0100

    Extract views and UI updates

--- Bullseye/views/Shapes.swift
+++ Bullseye/views/Shapes.swift
@@ -1,18 +1,18 @@
 //
-//  TextViews.swift
+//  Shapes.swift
 //  Bullseye
 //
-//  Created by Anastasiia Solomka on 12.11.2023.
+//  Created by Anastasiia Solomka on 11.11.2023.
 //
 
 import SwiftUI
 
-struct TextViews: View {
+struct Shapes: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    TextViews()
+    Shapes()
 }
