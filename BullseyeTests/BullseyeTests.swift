20fc42ee3c70e550b71452d3b8b32d721b8ebbee
Anastasiia Solomka <solomken@gmail.com>
2023-11-13 14:40:44 +0100

    add bonus points to score and tests

--- BullseyeTests/BullseyeTests.swift
+++ BullseyeTests/BullseyeTests.swift
@@ -30,4 +30,22 @@ final class BullseyeTests: XCTestCase {
         let score = game.points(sliderValue: guess)
         XCTAssertEqual(score, 95)
     }
+    
+    func testScoreExact() {
+        let guess = game.target
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 200)
+    }
+    
+    func testScoreClose() {
+        let guess = game.target + 2
+        let score = game.points(sliderValue: guess)
+        XCTAssertEqual(score, 98 + 50)
+    }
+    
+    func testNewRound() {
+        game.startNewRound(points: 100)
+        XCTAssertEqual(game.score, 100)
+        XCTAssertEqual(game.round, 2)
+    }
 }
