19f1d2699c145cbf9089aee91e5c4ec36b3d71f2
Anastasiia Solomka <solomken@gmail.com>
2023-11-21 14:44:42 +0100

    add leaderboard list with real data

--- Bullseye/models/Game.swift
+++ Bullseye/models/Game.swift
@@ -11,6 +11,18 @@ struct Game {
     var target = Int.random(in: 1...100)
     var score = 0
     var round = 1
+    var leaderboardEntries: [LeaderboardEntry] = []
+    
+    init(loadTestData: Bool = false) {
+        if loadTestData {
+            addToLeaderboard(score: 100)
+            addToLeaderboard(score: 148)
+            addToLeaderboard(score: 56)
+            addToLeaderboard(score: 200)
+            addToLeaderboard(score: 20)
+            addToLeaderboard(score: 149)
+        }
+    }
     
     func points(sliderValue: Int) -> Int {
         let difference = abs(sliderValue - target)
@@ -28,9 +40,17 @@ struct Game {
     }
     
     mutating func startNewRound(points: Int) {
+        addToLeaderboard(score: points)
         score += points
         round += 1
         target = Int.random(in: 1...100)
+    }
+    
+    mutating func addToLeaderboard(score: Int) {
+        leaderboardEntries.append(LeaderboardEntry(score: score, date: Date()))
+        leaderboardEntries.sort { entry1, entry2 in
+            entry1.score > entry2.score
+        }
     }
     
     mutating func restart() {
@@ -38,4 +58,9 @@ struct Game {
         round = 1
         target = Int.random(in: 1...100)
     }
+}
+
+struct LeaderboardEntry {
+    let score: Int
+    let date: Date
 }
